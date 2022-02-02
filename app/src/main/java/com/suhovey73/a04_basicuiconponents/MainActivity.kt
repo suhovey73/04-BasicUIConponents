a74ee8a02f283372e6760cf24a3ae9a03a80c594
suhovey73 <suhovey73@gmail.com>
2022-02-02 20:43:21 +0700

    Access to elements

--- app/src/main/java/com/suhovey73/a04_basicuiconponents/MainActivity.kt
+++ app/src/main/java/com/suhovey73/a04_basicuiconponents/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.suhovey73.a04_basicuiconponents
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.annotation.ColorInt
+import com.suhovey73.a04_basicuiconponents.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tvByFindViewById: TextView
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tvByFindViewById = findViewById(R.id.tvOnAccessBy_findViewById)
+
+        binding.btnChangePropertyByFindViewById.setOnClickListener {
+            tvByFindViewById.setTextColor(randomColor())
+        }
+
+        binding.btnChangePropertyByViewBinding.setOnClickListener {
+            binding.tvOnAccessByViewBinding.setTextColor(randomColor())
+        }
     }
+
+    private fun randomColor(): Int {
+        val color = Color.argb(255, Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+        return color
+    }
+
 }
