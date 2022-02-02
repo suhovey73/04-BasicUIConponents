d9592b5ad515009faa355486f3cbd2c0b3d72651
suhovey73 <suhovey73@gmail.com>
2022-02-02 12:16:22 +0700

    Project init

---
+++ app/src/main/java/com/suhovey73/a04_basicuiconponents/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.suhovey73.a04_basicuiconponents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
