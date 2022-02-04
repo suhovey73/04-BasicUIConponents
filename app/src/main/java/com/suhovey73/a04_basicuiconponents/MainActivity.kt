b176f07f314c9bdb7902821c41f4fd84d39d0450
suhovey73 <suhovey73@gmail.com>
2022-02-04 12:57:51 +0700

    Some parameters TextView

--- app/src/main/java/com/suhovey73/a04_basicuiconponents/MainActivity.kt
+++ app/src/main/java/com/suhovey73/a04_basicuiconponents/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.suhovey73.a04_basicuiconponents
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.TypedValue
+import com.suhovey73.a04_basicuiconponents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with (binding) {
+            btnChangeFontFamily.setOnClickListener {
+                val face = Typeface.create("casual", textView.typeface.style)
+                textView.typeface = face
+            }
+
+            btnChangeLinesCount.setOnClickListener {
+                textView.setLines(3)
+            }
+
+            btnChangeSize.setOnClickListener {
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,25F);
+            }
+
+            btnChangeStyle.setOnClickListener {
+                textView.setTypeface(textView.typeface, Typeface.BOLD);
+            }
+
+            btnChangeText.setOnClickListener {
+                textView.setLines(1)
+                textView.ellipsize = TextUtils.TruncateAt.END
+                textView.text = getString(R.string.test)
+            }
+        }
     }
 }
