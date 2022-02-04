b74b04d6e63d79b7149fab9548171a573deffce0
suhovey73 <suhovey73@gmail.com>
2022-02-04 17:19:31 +0700

    Created element ImageView

--- app/src/main/java/com/suhovey73/a04_basicuiconponents/MainActivity.kt
+++ app/src/main/java/com/suhovey73/a04_basicuiconponents/MainActivity.kt
@@ -2,10 +2,20 @@ package com.suhovey73.a04_basicuiconponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.suhovey73.a04_basicuiconponents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(getLayoutInflater()) // Need to use "inflaterLayout"
+
+        setContentView(binding.root)
+
+        binding.btnGetNextImage.setOnClickListener {
+
+        }
     }
 }
